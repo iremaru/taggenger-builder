92885b20146599703701da827cd34e88c2f0b387
iremaru <irene_marrero_ruiz@hotmail.com>
2023-12-16 18:24:25 +0000

    stable

--- src/shared/rack-label-105/rack-label-105.component.ts
+++ src/shared/rack-label-105/rack-label-105.component.ts
@@ -1,22 +1,64 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  AfterViewInit,
+  OnChanges,
+  Renderer2,
+} from '@angular/core';
 
 @Component({
   selector: 'app-rack-label-105',
   templateUrl: './rack-label-105.component.html',
   styleUrls: ['./rack-label-105.component.scss'],
 })
-export class RackLabel105Component implements OnInit {
+export class RackLabel105Component implements OnInit, AfterViewInit, OnChanges {
   @Input({ required: true }) familyName!: string;
   @Input({ required: true }) shortDescription!: string;
   @Input({ required: true }) size!: string;
   @Input({ required: true }) flavor!: string;
 
   @Input({ required: true }) qrText!: string;
+  @Input({ required: true }) currency!: string;
   @Input({ required: true }) price!: string;
 
-  constructor() {}
+  priceText!: HTMLElement;
+  priceText_DefaultFontSize = 36;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
-    console.log(this.qrText);
+    this.priceText = this.el.nativeElement.querySelector(
+      '#priceText'
+    ) as HTMLElement;
+  }
+
+  ngAfterViewInit() {
+    this.priceText_DefaultFontSize = parseInt(
+      window.getComputedStyle(this.priceText).fontSize
+    );
+
+    this.correctTextSize(this.priceText, this.priceText_DefaultFontSize);
+  }
+
+  ngOnChanges() {
+    if (this.priceText != null)
+      this.correctTextSize(this.priceText, this.priceText_DefaultFontSize);
+  }
+
+  /**Correct the text size so it don't overflow the box */
+  correctTextSize(element: HTMLElement, defaultFontSize: number) {
+    const elementWidth = element.offsetWidth;
+    const parentWidth = (element.parentElement as HTMLElement).offsetWidth;
+
+    const ratio = parentWidth / elementWidth;
+    if (ratio >= 1 && ratio <= 1.2) return;
+
+    const prevFontSize = parseInt(window.getComputedStyle(element).fontSize);
+    const newFontSize =
+      ratio < 1 ? Math.floor(ratio * prevFontSize) : defaultFontSize;
+
+    this.renderer.setStyle(element, 'font-size', newFontSize + 'px');
   }
 }
--- src/views/main-page/main-page.component.ts
+++ src/views/main-page/main-page.component.ts
@@ -7,35 +7,15 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./main-page.component.scss'],
 })
 export class MainPageComponent implements OnInit {
-  tagInfo!: FormGroup;
-  familyName = 'Fulano Benganez';
-  shortDescription = 'Un desarrollador';
-  size = 'Junior';
-  flavor = 'Backend';
-  //price = '20k';
+  familyName!: Event;
+  shortDescription!: Event;
+  size!: Event;
+  flavor!: Event;
   qrText!: Event;
   price!: Event;
+  currency!: Event;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.tagInfo = formBuilder.group({
-      familyName: this.familyName,
-      shortDescription: this.shortDescription,
-      size: this.size,
-      flavor: this.flavor,
-      //price: this.price,
-    });
-  }
+  constructor() {}
 
   ngOnInit() {}
-
-  public setFormData(): void {
-    this.familyName =
-      this.tagInfo?.get('familyName')?.value || 'Fulano Beganez';
-    this.shortDescription =
-      this.tagInfo?.get('shortDescription')?.value ||
-      'Tu picacódigo de confianza';
-    this.size = this.tagInfo?.get('size')?.value || 'Junior';
-    this.flavor = this.tagInfo?.get('flavor')?.value || 'Backend';
-    //this.price = this.tagInfo?.get('price')?.value || '20K';
-  }
 }
